2f0c34895c868e8d780c69162515010f8a31cf29
jpgard <joshua.patrick.gardner@gmail.com>
2018-11-09 11:34:47 -0800

    initial commit

---
+++ preprocessing/foldit.py
@@ -0,0 +1,12 @@
+"""Functions to preprocess foldit data into user-puzzle sequences"""
+
+import pandas as pd
+
+def get_pid_uid_sequences(infile, max_len=1000):
+    """
+    Fetch a hierarchical dictionary with keys [pid][uid] and values which are sequences of ac
+    :param infile:
+    :return:
+    """
+    #todo
+    return
