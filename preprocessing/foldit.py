e2b02113e56e1c7d02e7f3ba1569328c3cec688b
jpgard <joshua.patrick.gardner@gmail.com>
2018-11-09 17:24:13 -0800

    implementation of get_pid_uid_squences()

--- preprocessing/foldit.py
+++ preprocessing/foldit.py
@@ -1,12 +1,27 @@
 """Functions to preprocess foldit data into user-puzzle sequences"""
 
 import pandas as pd
+from collections import defaultdict
 
-def get_pid_uid_sequences(infile, max_len=1000):
+
+def get_pid_uid_sequences(infile, max_len=5000):
     """
-    Fetch a hierarchical dictionary with keys [pid][uid] and values which are sequences of ac
+    Fetch a hierarchical dictionary with keys [pid][uid] and values which are sequences of actions
     :param infile:
-    :return:
+    :param max_len: maximum length of sequence to store
+    :return: hierarchical dictionary with keys [pid][uid] and values which are np.ndarray containing sequences of actions
     """
-    #todo
-    return
+    pid_col = "puzzle_id"
+    uid_col = "user_id"
+    ts_col = "timestamp"
+    tool_col = "tool"
+    tool_code_col= "tool_code" #column to generate; numeric identifiers for tools
+    empty_event_value=-999
+    df = pd.read_csv(infile)
+    df[tool_col] = pd.Categorical(df[tool_col])
+    df[tool_code_col] = df[tool_col].cat.codes
+    pid_uid_seqs = defaultdict(dict)
+    uid_pid_values = df[[uid_col, pid_col]].drop_duplicates()
+    for ix,uid,pid in uid_pid_values.itertuples():
+        pid_uid_seqs[pid][uid] = df.loc[(df[uid_col] == uid) & (df[pid_col] == pid), tool_code_col].values[0:max_len]
+    return pid_uid_seqs
