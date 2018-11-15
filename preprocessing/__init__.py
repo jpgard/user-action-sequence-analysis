b5f1057cd2d91ac3a1369d9467cd872a36e7effd
jpgard <joshua.patrick.gardner@gmail.com>
2018-11-14 17:20:29 -0800

    removed ipdb call

--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -12,10 +12,9 @@ def preprocess_lstm_data(input_dict, max_len):
     pid_uid_seqs = [(pid, uid, input_dict[pid][uid]) for pid in input_dict.keys() for uid in input_dict[pid].keys()]
     n = len(pid_uid_seqs)
     X = pad_sequences([row[2] for row in pid_uid_seqs], maxlen=max_len, padding="post", truncating="post", value=-1)
-    import ipdb;ipdb.set_trace()
     X = to_categorical(X)
     # X = np.reshape(X, (n, max_len, 1)) # values for tuple are (num_obs, timeseries_length, observations_per_timestep/num_words_in_vocab); see https://machinelearningmastery.com/reshape-input-data-long-short-term-memory-networks-keras/
     # create Y, an array of (pid, uid) for each row in X
-    Y = np.array([(row[0], row[1]) for row in pid_uid_seqs]) 
+    Y = np.array([(row[0], row[1]) for row in pid_uid_seqs])
     assert Y.shape[0] == X.shape[0]
     return X, Y
